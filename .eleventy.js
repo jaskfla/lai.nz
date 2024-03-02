d283db22a47d3ce84b57eeddea8c3f009b1e3b74
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-03-02 13:14:57 +1300

    fix passthrough file paths

--- .eleventy.js
+++ .eleventy.js
@@ -3,16 +3,16 @@ const SOURCE_DIR = 'src'
 export default (eleventyConfig) => {
 	const staticFiles = [
 		// Directories
-		`/fonts`,
-		`/images`,
-		`/meta`,
-		`/style`,
+		`fonts`,
+		`images`,
+		`meta`,
+		`style`,
 
 		// Files
-		`/apple-touch-icon.png`,
-		`/favicon.ico`,
-		`/humans.txt`,
-		`/robots.txt`,
+		`apple-touch-icon.png`,
+		`favicon.ico`,
+		`humans.txt`,
+		`robots.txt`,
 	]
 	for (const file of staticFiles) {
 		eleventyConfig.addPassthroughCopy(`${SOURCE_DIR}/${file}`)
