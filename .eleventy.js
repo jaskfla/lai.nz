e77a99ebee6f8d36339cf1f286d444803d25a56d
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-04-03 22:02:09 +1300

    format code

--- .eleventy.js
+++ .eleventy.js
@@ -1,26 +1,26 @@
-const SOURCE_DIR = 'src'
+const SOURCE_DIR = "src";
 
 export default (eleventyConfig) => {
-	const staticFiles = [
-		// Directories
-		`fonts`,
-		`images`,
-		`meta`,
-		`style`,
+  const staticFiles = [
+    // Directories
+    "fonts",
+    "images",
+    "meta",
+    "style",
 
-		// Files
-		`apple-touch-icon.png`,
-		`favicon.ico`,
-		`humans.txt`,
-		`robots.txt`,
-	]
-	for (const file of staticFiles) {
-		eleventyConfig.addPassthroughCopy(`${SOURCE_DIR}/${file}`)
-	}
+    // Files
+    "apple-touch-icon.png",
+    "favicon.ico",
+    "humans.txt",
+    "robots.txt",
+  ];
+  for (const file of staticFiles) {
+    eleventyConfig.addPassthroughCopy(`${SOURCE_DIR}/${file}`);
+  }
 
-	return {
-		dir: {
-			input: SOURCE_DIR,
-		},
-	}
-}
+  return {
+    dir: {
+      input: SOURCE_DIR,
+    },
+  };
+};
