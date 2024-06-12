e4b75f59ec1ae716c9e01ec13afef581f2cd5e99
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-06-13 00:22:14 +1200

    `/now` page

---
+++ src/_data/coverflow.js
@@ -0,0 +1,32 @@
+export default [
+	{
+		title: 'The Blessed Unrest, by Sarah Bareilles',
+		coverPath: '/images/cover-art/sara_bareilles__blessed-unrest',
+		link: 'https://songwhip.com/sara-bareilles/the-blessed-unrest',
+	},
+	{
+		title: 'One Night Only, by Lady Sophie',
+		coverPath: '/images/cover-art/lady-sophie__one-night-only-500',
+		link: 'https://songwhip.com/ladysophie/one-night-only-to-the-boy-who-broke-my-heart',
+	},
+	{
+		title: 'Novo Amor, by Birthplace',
+		coverPath: '/images/cover-art/novo-amor__birthplace',
+		link: 'https://songwhip.com/novo-amor/birthplace',
+	},
+	{
+		title: 'Echo of Youth, by Winterbourne',
+		coverPath: '/images/cover-art/winterbourne__echo-of-youth',
+		link: 'https://songwhip.com/winterbourne/echo-of-youth',
+	},
+	{
+		title: 'Win Son Presents: A Taiwanese American Cookbook',
+		coverPath: '/images/cover-art/win-son-cookbook-500',
+		link: 'https://winsonbrooklyn.com/cookbook',
+	},
+	{
+		title: 'Our Flag Means Death',
+		coverPath: '/images/cover-art/our-flag-means-death-500',
+		link: 'https://www.justwatch.com/nz/tv-show/our-flag-means-death',
+	},
+];
