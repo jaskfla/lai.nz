25010c742619bd0fdd9ef514ad6fb5d8945895ad
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-08-06 19:32:52 +1200

    🫡🌆 Songwhip

--- src/_data/coverflow.js
+++ src/_data/coverflow.js
@@ -2,22 +2,22 @@ export default [
 	{
 		title: 'The Blessed Unrest, by Sarah Bareilles',
 		coverPath: '/images/cover-art/sara_bareilles__blessed-unrest',
-		link: 'https://songwhip.com/sara-bareilles/the-blessed-unrest',
+		link: 'https://album.link/i/1440504715',
 	},
 	{
 		title: 'One Night Only, by Lady Sophie',
 		coverPath: '/images/cover-art/lady-sophie__one-night-only-500',
-		link: 'https://songwhip.com/ladysophie/one-night-only-to-the-boy-who-broke-my-heart',
+		link: 'https://album.link/i/1541396407',
 	},
 	{
 		title: 'Novo Amor, by Birthplace',
 		coverPath: '/images/cover-art/novo-amor__birthplace',
-		link: 'https://songwhip.com/novo-amor/birthplace',
+		link: 'https://album.link/i/1672227185',
 	},
 	{
 		title: 'Echo of Youth, by Winterbourne',
 		coverPath: '/images/cover-art/winterbourne__echo-of-youth',
-		link: 'https://songwhip.com/winterbourne/echo-of-youth',
+		link: 'https://album.link/i/1444693578',
 	},
 	{
 		title: 'Win Son Presents: A Taiwanese American Cookbook',
