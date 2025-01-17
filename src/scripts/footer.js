441a132bb36959783bfbfb6cb3df1c31a1060b28
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-01-18 10:54:22 +1300

    new quips

--- src/scripts/footer.js
+++ src/scripts/footer.js
@@ -19,11 +19,13 @@ document.getElementById('thanks').outerHTML = pickOne([
 
 const quips = [
 	'<a href="http://wisdom.limo" target="_blank">Looking for the chimneys</a>',
+	'<a href="https://hackers.town/@calcifer/113818863859989960" target="_blank">Stay whimsical, silly geese</a>',
 	'<a href="https://nerdfighteria.info/v/307598550" target="_blank">Memento mori</a>&nbsp;😊',
 	'<a href="https://overcast.fm/+BmEPOwtek" target="_blank">Keep moving <em>and</em> get out of the way</a>',
 	'<a href="https://song.link/i/1444693586" target="_blank" title="Puzzle by Winterbourne">It’s a big fucking ladder to take a rung at a time</a>',
 	'<a href="https://song.link/i/1553604480" target="_blank" title="Talk to Me by Cavetown">You don’t have to be a prodigy to be unique</a>',
 	'<a href="https://www.goodreads.com/book/show/42190.Wabi_Sabi" target="_blank" title="Wabi-Sabi by Leonard Koren">Pare down to the essence, but don’t remove the poetry</a>',
+	'<a href="https://www.youlooknicetoday.com/articles/hello" target="_blank">It’s always two things with me</a>',
 	'<a href="https://youtu.be/FyQi79aYfxU?t=73" target="_blank">Don’t forget to be awesome!</a>&nbsp;🎃🐧',
 	'<span title="Any ideas? Please contact me!">Still brainstorming a <a href="http://www.thehomeland.org/blog/2018/04/05/masterlist-of-name-specific-sign-offs-dear-hank-john" target="_blank">name-specific sign-off</a>,&emsp;<span class="caps">—Jasper</span></span>',
 	'Anticipating the next episode of <a class="caps" href="https://www.relay.fm/bonanza" target="_blank">BONANZA</a>&nbsp;🛟',
