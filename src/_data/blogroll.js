899c4c48dd9653c04edcaa0fc32c3872934a06ca
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-06-14 23:19:33 +1200

    refactor blogroll

---
+++ src/_data/blogroll.js
@@ -0,0 +1,78 @@
+export default [
+	{
+		title: '5ives',
+		url: 'https://www.5ives.com',
+	},
+	{
+		title: '43&nbsp;Folders',
+		url: 'http://www.43folders.com',
+	},
+	{
+		title: 'Adam Argyle',
+		url: 'https://nerdy.dev',
+	},
+	{
+		title: 'Ahmad Shadeed',
+		url: 'https://ishadeed.com',
+	},
+	{
+		title: 'Arun Venkatesan',
+		url: 'https://arun.is/blog',
+	},
+	{
+		title: 'Bartosz Ciechanowski',
+		url: 'https://ciechanow.ski',
+	},
+	{
+		title: 'The Cascade',
+		url: 'https://csscade.com',
+	},
+	{
+		title: 'Daring Fireball',
+		url: 'https://daringfireball.net',
+	},
+	{
+		title: 'Heydon Pickering',
+		url: 'https://heydonworks.com/latest',
+	},
+	{
+		title: 'Jim Nielsen',
+		url: 'https://blog.jim-nielsen.com',
+	},
+	{
+		title: 'Josh Comeau',
+		url: 'https://www.joshwcomeau.com',
+	},
+	{
+		title: 'The <abbr title="Hypertext Markup Language">HTML</abbr> Review',
+		url: 'https://thehtml.review',
+	},
+	{
+		title: 'Jonnie Hallman',
+		url: 'https://destroytoday.com/blog',
+	},
+	{
+		title: 'Manuel Moreale',
+		url: 'https://manuelmoreale.com',
+	},
+	{
+		title: 'Not a Number',
+		url: 'https://www.nan.fyi',
+	},
+	{
+		title: 'Piccalilli',
+		url: 'https://piccalil.li/blog',
+	},
+	{
+		title: 'Pixel Envy',
+		url: 'https://pxlnv.com',
+	},
+	{
+		title: 'Robin Rendle',
+		url: 'https://robinrendle.com',
+	},
+	{
+		title: 'Sara Soueidan',
+		url: 'https://www.sarasoueidan.com/blog',
+	},
+];
--- src/blogroll.md
+++ src/blogroll.md
@@ -7,22 +7,10 @@ permalink: blogroll.html
 My grand list of top-shelf follows. (It’s not that grand, but these are well
 interesting.)
 
-- [5ives](https://www.5ives.com)
-- [43 Folders](http://www.43folders.com)
-- [Adam Argyle](https://nerdy.dev)
-- [Ahmad Shadeed](https://ishadeed.com)
-- [Arun Venkatesan](https://arun.is/blog)
-- [Bartosz Ciechanowski](https://ciechanow.ski)
-- [The Cascade](https://csscade.com)
-- [Daring Fireball](https://daringfireball.net)
-- [Heydon Pickering](https://heydonworks.com/latest)
-- [Jim Nielsen](https://blog.jim-nielsen.com)
-- [Josh Comeau](https://www.joshwcomeau.com)
-- [The <abbr title="Hypertext Markup Language">HTML</abbr> Review](https://thehtml.review)
-- [Jonnie Hallman](https://destroytoday.com/blog)
-- [Manuel Moreale](https://manuelmoreale.com)
-- [Not a Number](https://www.nan.fyi)
-- [Piccalilli](https://piccalil.li/blog)
-- [Pixel Envy](https://pxlnv.com)
-- [Robin Rendle](https://robinrendle.com)
-- [Sara Soueidan](https://www.sarasoueidan.com/blog)
+<ul>
+	{% for blog in blogroll %}
+	<li>
+		<a href="{{ blog.url }}" rel="external" target="_blank">{{ blog.title }}</a>
+	</li>
+	{% endfor %}
+</ul>
