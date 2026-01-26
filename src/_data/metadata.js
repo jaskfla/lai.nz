b01ee7053fe721780c8e253052dea97f6189f796
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-01-26 17:36:41 +1300

    use `pkg.homepage`

--- src/_data/metadata.js
+++ src/_data/metadata.js
@@ -4,7 +4,6 @@ export default {
 	name: 'Jasper Lai',
 	domain: 'lai.nz',
 	email: '&#106;&#64;&#108;&#97;&#105;&#46;&#110;&#122;',
-	url: 'https://lai.nz',
 	description:
 		'Jasper is software engineering student at the University of Auckland, and a UX/UI design graduate.',
 	repoUrl: 'https://github.com/jaskfla/lai.nz',
