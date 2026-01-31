0a54838f5cfce17cd1aafeb1f7da83da9ab47704
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-01-31 18:00:46 +1300

    iamrob.in

--- src/_data/personal-sites.js
+++ src/_data/personal-sites.js
@@ -51,6 +51,7 @@ export default [
 	'https://wojtek.im',
 	'https://www.adithnarein.com',
 	'https://www.alasdairmonk.com',
+	'https://www.iamrob.in',
 	'https://www.joshuawootonn.com',
 	'https://www.yasmins.site',
 	'https://yannglt.com',
