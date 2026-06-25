cd8a51dffe3a70d25e595b7e3ede124e46fc3efd
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-06-25 21:48:53 +1200

    closing paren

--- src/_data/metadata.js
+++ src/_data/metadata.js
@@ -5,7 +5,7 @@ export default {
 	domain: 'lai.nz',
 	email: '&#106;&#64;&#108;&#97;&#105;&#46;&#110;&#122;',
 	description:
-		'Has annoying opinions about fonts. (Also making healthcare software at Beyond Essential Systems.',
+		'Has annoying opinions about fonts. (Also making healthcare software at Beyond Essential Systems.)',
 	repoUrl: 'https://github.com/jaskfla/lai.nz',
 	/** @see https://stackoverflow.com/a/34518749/5323344 */
 	commitHash: execSync('git rev-parse HEAD').toString().trim(),
