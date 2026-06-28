8e77029ee7237157d17b56a371dd82426112d2f5
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-06-28 22:20:22 +1200

    `maggieappleton.com`

--- src/_data/personal-sites.js
+++ src/_data/personal-sites.js
@@ -22,6 +22,7 @@ const urls = [
 	'https://kuntz.io',
 	'https://lisamariamarquis.com',
 	'https://lynnandtonic.com',
+	'https://maggieappleton.com',
 	'https://marijanapav.com',
 	'https://medienbaecker.com',
 	'https://mifsud.org',
